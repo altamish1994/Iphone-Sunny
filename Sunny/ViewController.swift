88ef5a8dccc9a18f39bc2bcf2b07f9031985504e
Altamish Arif <altamish1994@gmail.com>
2015-07-29 08:01:51 +0500

    Initial Commit

---
+++ Sunny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sunny
+//
+//  Created by Altamish Arif on 29/07/2015.
+//  Copyright (c) 2015 Altamish Arif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
